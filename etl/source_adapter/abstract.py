7e6f13866cae673cc032b8ee1d973141b5ac3dec
Lasse Karls <l_karls@outlook.com>
2023-05-03 22:26:55 +0200

    Need to fix this Timeperiod stuff in the future

--- etl/source_adapter/abstract.py
+++ etl/source_adapter/abstract.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d588d27a5eebe06d7289bee70a35a174f1cc4e98ff4a5b0d41cc7a85ef40ffcb
-size 9268
+oid sha256:be852a0241d69d152b96057cf599d68bd8631fff076a28abc95331c78340ba2e
+size 9311
