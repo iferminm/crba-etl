852551796d2f8e8097e537fcd4b98c7d350d3eb4
Lasse Karls <l_karls@outlook.com>
2023-05-20 13:46:19 +0200

    CRBA-106

--- etl/source_adapter/idmc.py
+++ etl/source_adapter/idmc.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bfa550902ca8d541a3d9f5132ce8627736530ebafac114cf3d05ede448c4583
-size 1558
+oid sha256:f04f9a16046ea0e9ffb39dab8acbe1aa22128b0e809479fd4273da6edcdb36ca
+size 1570
