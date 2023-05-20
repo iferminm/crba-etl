ba68a5088e78edff8e506fcb7eca87e2c2342849
Lasse Karls <l_karls@outlook.com>
2023-05-20 21:38:11 +0200

    Migrate Firt Clima Watch source to API

--- etl/methology/column_mapping.py
+++ etl/methology/column_mapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22504edaed81e2d9ab55ca33dd623a9a3bcba25f98c41eda80254a16268d04f2
-size 13064
+oid sha256:20b8f9a0d2df5c568a5336f6821fad3ef38aeb2674ede6f482c448bab5346cbc
+size 13081
--- etl/source_adapter/manual.py
+++ etl/source_adapter/manual.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2a5976658cc31373879387596b5daf58c7fe1eefeabb5d2b033e1a5216f1914
-size 12743
+oid sha256:6856d2ed6292871d423d46fc33e8bd0eacc8786ced064790b0b6262f40e7b6a5
+size 11804
