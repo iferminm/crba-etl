cd57461b6e66b33984999d8de3770f0eecdbc291
Lasse Karls <l_karls@outlook.com>
2023-05-20 15:04:46 +0200

    Update URL Endpoint for I-87

--- etl/methology/column_mapping.py
+++ etl/methology/column_mapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51c54b9965a9c7d7cff34b803e83ff2e2addf8f5360d7ff6c63041a716a4e0af
-size 13023
+oid sha256:22504edaed81e2d9ab55ca33dd623a9a3bcba25f98c41eda80254a16268d04f2
+size 13064
--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1965061660d6065b5fd4c8e9e7bb2db64552c995cd5cb83921c4bf7b96b3e970
-size 3285
+oid sha256:01df2b858659d00ba4869af34426d197fb095a2d38ae5aeaf7eae92c10b56f15
+size 3119
