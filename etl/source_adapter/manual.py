3f82bf2544c6f810d8a06db027b062004ad04912
Lasse Karls <l_karls@outlook.com>
2023-05-23 22:45:25 +0200

    Updated S-100. Introduce normalization to 100000 for Blueprint. Delete duplicate information in Indicator.json

--- etl/source_adapter/manual.py
+++ etl/source_adapter/manual.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6856d2ed6292871d423d46fc33e8bd0eacc8786ced064790b0b6262f40e7b6a5
-size 11804
+oid sha256:4c7dd75a2ccaf0d707ae37798b17213970e15bf00f89fd1d536925dbccc0a69b
+size 12584
