c12bb76bc0ea6060f99675a8b73f91990e0c8b4f
Lasse Karls <l_karls@outlook.com>
2023-05-23 19:48:46 +0200

    S-193: Add DIME AGE GROUPS and filter for right one

--- etl/methology/value_mapping.py
+++ etl/methology/value_mapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d8edd22fb78c8778e6e9adf53128d388870863c94b1c8589a0cd8aa21f790d2
-size 7685
+oid sha256:7d5cb70b742d89ab0cd6cd58e881e47d7b3d97d54b4813e73b75daeac1e1b068
+size 7829
