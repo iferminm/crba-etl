6cbea2e1489a9ea76ceef17e33909daf0f81a9a2
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-05-23 21:33:40 +0200

    Make windows compatible

--- etl/conf.py
+++ etl/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f3106f0a7ece6a28b04411d4816d6d64027ea2b55b784569d195c76a46e3e6f
-size 10683
+oid sha256:56066cb8f3c9e2b238146dcbdd68da0e3510ff5f6f2ce37bd87bc712445521e2
+size 10701
