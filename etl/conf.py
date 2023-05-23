1a21237d59d62176654c1dc5ec92e7c11d0e6931
Lasse Karls <l_karls@outlook.com>
2023-05-23 22:15:30 +0200

    Because of changing value id in source selection. Resturcture crba_report definition. Commit: e18c3d7

--- etl/conf.py
+++ etl/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56066cb8f3c9e2b238146dcbdd68da0e3510ff5f6f2ce37bd87bc712445521e2
-size 10701
+oid sha256:88b97661c537964f09d60c2801c9bc268bb9d6e2c4953b7b18af809ec723856d
+size 10642
