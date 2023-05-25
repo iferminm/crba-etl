5ba551932929c8c6320cb8d243c71a64555c5312
Lasse Karls <l_karls@outlook.com>
2023-05-25 11:52:40 +0200

    Fix Source S-140

--- etl/source_adapter/who.py
+++ etl/source_adapter/who.py
@@ -46,39 +46,35 @@ class S_157(SourceAdapter):
 
     def _download(self):
         csv_data = SourceAdapter.api_request(self.endpoint).text
-        dataframe = pd.read_csv(StringIO(csv_data), sep=",")
+        self.dataframe = pd.read_csv(StringIO(csv_data), sep=",")
 
         # We only have the population data for both sexes, so discrd other dimensionsubgroups
-        dataframe = dataframe.loc[dataframe.SEX == "BTSX"]
+        self.dataframe = self.dataframe.loc[self.dataframe.SEX == "BTSX"]
 
-        s_157 = self.config.un_pop_tot.merge(
-            right=dataframe,
+        self.dataframe = self.config.un_pop_tot.merge(
+            right=self.dataframe,
             how="right",
             # on="ISO3_YEAR"
             left_on=["COUNTRY_ISO_3", "TIME_PERIOD"],
-            right_on=["COUNTRY", "Year"],
+            right_on=["COUNTRY", "YEAR"],
         )
 
-        # Join population data to raw data
-        # s_157 = self.config.un_pop_tot.merge(
-        #     right=dataframe, how="right", left_index=True, right_on="COUNTRY"
-        # )
-
         # Compute target raw observation value
-        s_157["RAW_OBS_VALUE"] = (
-                s_157["Numeric"] / (s_157["population"]) * 100
+        self.dataframe["RAW_OBS_VALUE"] = (
+                self.dataframe["Numeric"] / (self.dataframe["population"]) * 100
         )
 
         # Add attribute
-        s_157[
+        self.dataframe[
             "ATTR_UNIT_MEASURE"
         ] = "The burden of disease attributable to ambient air pollution expressed as Number of deaths of children under 5 years per 100.000 children under 5 years. Note: Data about deaths drawm from WHO (https://apps.who.int/gho/data/node.imr.AIR_4?lang=en), refering to year 2016. Data about population under 5 years drawn from UNICEF (https://data.unicef.org/resources/data_explorer/unicef_f/?ag=UNICEF&df=GLOBAL_DATAFLOW&ver=1.0&dq=.DM_POP_U5..&startPeriod=2008&endPeriod=2018), referring to year 2018. Due to a lack of matching data, the WHO data from 2016 had to be normalized with population data from 2018."
 
         # Rename clumns
-        s_157 = s_157.rename(
+        self.dataframe = self.dataframe.rename(
             columns={
                 "TIME_PERIOD": "time_period_not_used",
                 "Display Value": "obs_value_no_used_2",
+                "COUNTRY": "country_not_used"
             }
         )
 
@@ -89,4 +85,4 @@ class S_157(SourceAdapter):
         #     display_value_col="Display Value",
         # )
 
-        return s_157
+        return self.dataframe
