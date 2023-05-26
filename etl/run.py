abd4a612322e5d4b09a0ad65636881c0d20832ab
Lasse Karls <l_karls@outlook.com>
2023-05-26 15:53:46 +0200

    Add code for calculating the final scores

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01df2b858659d00ba4869af34426d197fb095a2d38ae5aeaf7eae92c10b56f15
-size 3119
+oid sha256:b6132c5535288f996b3363a13acc0c513041a377ec60e445c73be5829beb9584
+size 9471
