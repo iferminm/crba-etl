7faa1f64b1aa0990167d58a33b9eaca650f00a22
Lasse Karls <l_karls@outlook.com>
2023-05-26 16:45:21 +0200

    Revert "Migrate Firt Clima Watch source to API" The API do not provide historical Data. Therfore the Blueprint File Approche is choosen
    This reverts commit a8dfe983f42c6cfaa387107e5fc20aadafbaf17f.

--- etl/source_adapter/cwd.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:043262f4fac0dafd105b38d47d3e6a59ac4184fc73aae132950317d8374e948b
-size 2842
--- etl/source_adapter/manual.py
+++ etl/source_adapter/manual.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34421bf8e0be5287063f9799f824c66d9824d0e525fc61f6d3a7579c82fd9e44
-size 12628
+oid sha256:35fd3001dffddbcf95d3bc83953c8327c3839ab224af999af8df015c673e30e0
+size 13567
