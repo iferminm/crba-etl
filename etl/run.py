5e522a456558b15181d6b2c46000bbaaac0ceadf
Lasse Karls <l_karls@outlook.com>
2023-05-26 15:53:46 +0200

    Add code for calculating the final scores

--- etl/run.py
+++ etl/run.py
@@ -1,5 +1,7 @@
+import csv
 import importlib
 import logging
+import re
 import warnings
 from typing import Type
 
@@ -7,6 +9,7 @@ import pandas as pd
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
+from etl.methology import country_crba_list
 from etl.source_adapter import ExtractionError
 
 run_logger = logging.getLogger("etl.progress_logger")
@@ -38,15 +41,23 @@ def build_combined_normalized_csv(config):
                     list(config.crba_report_definition.iterrows()), dynamic_ncols=True
             ):
                 try:
-                    row = row.dropna().to_dict()
-                    extractor = dynamic_load(row["EXTRACTOR_CLASS"])(config, **row)
-                    extractors[row["SOURCE_ID"]] = extractor
-                    extractor.build()
-                    # More IMportant then simple INFO logs but less importend the download infos
-                    df = extractor.get()
-                    run_logger.info(
-                        msg=f"Source {row['SOURCE_ID']} extract with {df.shape[0] if df is not None else 0} rows::: {extractor.__class__.__name__}",
-                    )
+                    #TODO Make as sys arg
+                    cached = True
+                    if not cached:
+                        row = row.dropna().to_dict()
+                        extractor = dynamic_load(row["EXTRACTOR_CLASS"])(config, **row)
+                        extractors[row["SOURCE_ID"]] = extractor
+                        extractor.build()
+                        # More IMportant then simple INFO logs but less importend the download infos
+                        df = extractor.get()
+                        run_logger.info(
+                            msg=f"Source {row['SOURCE_ID']} extract with {df.shape[0] if df is not None else 0} rows::: {extractor.__class__.__name__}",
+                        )
+                    else:
+                        df = pd.read_csv(
+                            config.indicator_output / str(row["SOURCE_ID"] + "_" + row["INDICATOR_ID"] + ".csv"),
+                            sep=";"
+                        )
                     extractions_data.append(df)
                     sucesses += 1
                 except ExtractionError as ex:
@@ -66,10 +77,197 @@ def build_combined_normalized_csv(config):
                     )
                 except TypeError as ex:
                     run_logger.exception(ex)
+                except FileNotFoundError as ex:
+                    run_logger.exception(ex)
+
 
     # Store Indicator for inspection
     print(f"Number of sucesses{sucesses}")
-    return (pd.concat(extractions_data, axis=0, ignore_index=True), "")
+    return pd.concat(extractions_data, axis=0, ignore_index=True)
+
+
+def aggregate_combined_normalized_csv(config, combined_normalized_csv):
+    # Idenify all dimension columns in combined dataframe
+
+    available_dim_cols = []
+    for col in combined_normalized_csv.columns:
+        dim_col = re.findall("DIM_.+", col)
+        # print(dim_col)
+        if len(dim_col) == 1:
+            available_dim_cols += dim_col
+
+    # Fill _T for all NA values of dimension columns
+    combined_normalized_csv[available_dim_cols] = combined_normalized_csv[
+        available_dim_cols
+    ].fillna(value="_T")
+
+    # Double check if there are duplicate countries
+    run_logger.info(
+        f"This is the number of duplicate rows:{sum(combined_normalized_csv.duplicated())}"
+    )
+
+    # print(combined_normalized_csv.loc[combined_normalized_csv.duplicated(), ['COUNTRY_ISO_3','INDICATOR_NAME', 'INDICATOR_CODE']])
+    # combined_normalized_csv = combined_normalized_csv.drop_duplicates() # uncomment if want to delete duplicates (but check where they come from first)
+
+    # Check that all indicators have been processed
+    # TODO Probably will allways fail as long there are any error with the sources
+    # assert config.source_config.shape[0] == len(build_combined_normalized_csv.INDICATOR_CODE.unique())
+
+    # Create category score
+    aggregated_scores_dataset = (
+        combined_normalized_csv.loc[
+            combined_normalized_csv["COUNTRY_ISO_3"] != "XKX",
+            [
+                "COUNTRY_ISO_3",
+                "SCALED_OBS_VALUE",
+                "INDICATOR_INDEX",
+                "INDICATOR_ISSUE",
+                "INDICATOR_CATEGORY",
+            ],
+        ]
+        .groupby(
+            by=[
+                "COUNTRY_ISO_3",
+                "INDICATOR_CATEGORY",
+                "INDICATOR_ISSUE",
+                "INDICATOR_INDEX",
+            ],
+            as_index=False,
+        )
+        .mean()
+        .rename(columns={"SCALED_OBS_VALUE": "CATEGORY_ISSUE_SCORE"})
+    )
+
+    # # # # Introduce weighting: duplicate all index_issues who belong to category outcome or enforcement
+    aggregated_scores_dataset = aggregated_scores_dataset.append(
+        aggregated_scores_dataset.loc[
+            (aggregated_scores_dataset["INDICATOR_CATEGORY"] == "Outcome")
+            | (aggregated_scores_dataset["INDICATOR_CATEGORY"] == "Enforcement")
+            ]
+    )
+
+    # # # # # # # Issue score
+    temp = (
+        aggregated_scores_dataset.groupby(
+            by=["COUNTRY_ISO_3", "INDICATOR_ISSUE", "INDICATOR_INDEX"], as_index=False
+        )
+        .mean()
+        .rename(columns={"CATEGORY_ISSUE_SCORE": "ISSUE_INDEX_SCORE"})
+    )
+
+    # Drop duplicates again
+    temp = temp.drop_duplicates()
+
+    # # Add risk category
+    # Define list of percentiles
+    percentile_33 = temp["ISSUE_INDEX_SCORE"].quantile(0.333)
+
+    percentile_66 = temp["ISSUE_INDEX_SCORE"].quantile(0.667)
+
+    # Add column indicating risk category
+    temp.loc[
+        temp["ISSUE_INDEX_SCORE"] < percentile_33, "ISSUE_INDEX_RISK_CATEGORY"
+    ] = "High risk"
+
+    temp.loc[
+        temp["ISSUE_INDEX_SCORE"] > percentile_66, "ISSUE_INDEX_RISK_CATEGORY"
+    ] = "Low risk"
+
+    temp.loc[
+        (temp["ISSUE_INDEX_SCORE"] > percentile_33)
+        & (temp["ISSUE_INDEX_SCORE"] < percentile_66),
+        "ISSUE_INDEX_RISK_CATEGORY",
+    ] = "Medium risk"
+
+    # # # # # #  Index score
+    temp_2 = (
+        temp.groupby(by=["COUNTRY_ISO_3", "INDICATOR_INDEX"], as_index=False)
+        .mean()
+        .rename(columns={"ISSUE_INDEX_SCORE": "INDEX_SCORE"})
+    )
+
+    # # Add risk category
+    # Define list of percentiles
+    percentile_33 = temp_2["INDEX_SCORE"].quantile(0.333)
+
+    percentile_66 = temp_2["INDEX_SCORE"].quantile(0.667)
+
+    # Add column indicating risk category
+    temp_2.loc[
+        temp_2["INDEX_SCORE"] < percentile_33, "INDEX_RISK_CATEGORY"
+    ] = "High risk"
+
+    temp_2.loc[
+        temp_2["INDEX_SCORE"] > percentile_66, "INDEX_RISK_CATEGORY"
+    ] = "Low risk"
+
+    temp_2.loc[
+        (temp_2["INDEX_SCORE"] > percentile_33)
+        & (temp_2["INDEX_SCORE"] < percentile_66),
+        "INDEX_RISK_CATEGORY",
+    ] = "Medium risk"
+
+    # # # # # Overall score
+    temp_3 = (
+        temp_2.groupby(
+            by=[
+                "COUNTRY_ISO_3",
+            ],
+            as_index=False,
+        )
+        .mean()
+        .rename(columns={"INDEX_SCORE": "OVERALL_SCORE"})
+    )
+
+    # Join all aggregated score together
+    aggregated_scores_dataset = (
+        aggregated_scores_dataset.merge(
+            right=temp,
+            on=[
+                "COUNTRY_ISO_3",
+                "INDICATOR_ISSUE",
+                "INDICATOR_INDEX",
+            ],
+        )
+        .merge(right=temp_2, on=["COUNTRY_ISO_3", "INDICATOR_INDEX"])
+        .merge(
+            right=temp_3,
+            on=[
+                "COUNTRY_ISO_3",
+            ],
+        )
+        .merge(right=country_crba_list, on="COUNTRY_ISO_3")
+        .drop(["COUNTRY_NAME", "COUNTRY_ISO_2"], axis=1)
+    )
+
+    crba_final = combined_normalized_csv.merge(
+        right=aggregated_scores_dataset,
+        on=[
+            "COUNTRY_ISO_3",
+            "INDICATOR_CATEGORY",
+            "INDICATOR_ISSUE",
+            "INDICATOR_INDEX",
+        ],
+        how="left",
+    )
+
+    # Did not join on entire composite key on left, must drop dupliates
+    crba_final = crba_final.drop_duplicates()
+
+    # Export combined cleansed dataframe as a sample
+    crba_final.to_csv(
+        path_or_buf=config.output_dir / "crba_final.csv",
+        sep=";",
+        index=False,
+    )
+
+    aggregated_scores_dataset.to_csv(
+        path_or_buf=config.output_dir / "aggregated_scores.csv",
+        sep=";",
+        quoting=csv.QUOTE_ALL,
+    )
+
+    return crba_final, aggregated_scores_dataset
 
 
 def run(config):
@@ -78,5 +276,6 @@ def run(config):
     This is to achieve better modular executability.The loss performace is accepted.
 
     """
-    build_combined_normalized_csv(config)
-    pass
+    combined_normalized_csv = build_combined_normalized_csv(config)
+
+    aggregate_combined_normalized_csv(config, combined_normalized_csv)
