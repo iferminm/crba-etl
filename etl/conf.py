7019a7abc9e2a517697b65cede9013885f8518a5
Lasse Karls <l_karls@outlook.com>
2023-06-12 21:49:39 +0200

    CRBA-161

--- etl/conf.py
+++ etl/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17dd7325be87fb827943ee63393ebe62f8aed818a92cc36c370bb361626bc718
-size 10543
+oid sha256:e485487b9b49c6e55ff7083fceda7898166e377f9389997022a8d426c37ca2db
+size 10548
