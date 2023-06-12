58db01188ed498b2d0817b0ebb84203eb391e059
Lasse Karls <l_karls@outlook.com>
2023-06-12 22:06:00 +0200

    CRBA-162

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:361fe75c65ca2386c7785c67f93fc8e9d83289dd247b8ba1b8637d26fc7a9811
-size 9636
+oid sha256:486a86d5830497eb6f7c716761a72ff54e550374f856ee920a90d16868bbdf3f
+size 9851
