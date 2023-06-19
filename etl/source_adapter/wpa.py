40b87e57d53bfd2ed281a44d115d170f45836bf0
Lasse Karls <l_karls@outlook.com>
2023-06-19 19:44:17 +0200

    Correct WPA Code

--- etl/source_adapter/wpa.py
+++ etl/source_adapter/wpa.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c83e23dac319125423a3dd1d81094f07359b7654b2ba7d54dcd34c923f5d254e
-size 4681
+oid sha256:0441ec075461998ad5189d71b6cc8bc0f6cf479c80d58d4f7aea398bf9649ed3
+size 4696
