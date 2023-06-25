2f0b35a1948a3c53ae60c09fe6234c481b8e82b3
Lasse Karls <l_karls@outlook.com>
2023-06-25 20:18:41 +0200

    Do refactoring of WPA

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14ba55fef1b3d1f8fbf5a602e13477f79d796a9e162df6e687b1090b9a63cab0
-size 12145
+oid sha256:95d2cce3084ac9e11a97752178bfb6ef5819acafcf7a0269cc33d19ba47b711a
+size 12157
