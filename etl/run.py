56369fe132aed8f7895debb7065aa47fde9ffbd6
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-06-25 17:21:50 +0200

    CRBA-164: Bump up CRBA version

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85defa54eb57e35685d0a8ca5ad2a67d3af38e7bc054b7f50cb38331ef5bbabd
+oid sha256:14ba55fef1b3d1f8fbf5a602e13477f79d796a9e162df6e687b1090b9a63cab0
 size 12145
