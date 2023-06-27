6b45d757917bf477bd72804080561c77a46647df
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-06-27 10:26:59 +0200

    NOTICKET: Improve visualisations

--- validation/comparison_of_final_files/compare_two_final_csvs.py
+++ validation/comparison_of_final_files/compare_two_final_csvs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76db88fc5bbc73e6e3277dd26b05da338b69ebb4da5873b6b01b6c7ec0dd60e3
-size 9969
+oid sha256:5322ea9d3797b556805a06150c755f9acb04f8606726fb054b706d3fde679e2e
+size 10267
