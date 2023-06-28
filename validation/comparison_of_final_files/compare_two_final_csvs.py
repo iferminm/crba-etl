c1330c4c5d3e2d420d0d1a3133663330bcb65f06
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-06-28 20:14:28 +0200

    NOTICKET: Add ocomparer function

--- validation/comparison_of_final_files/compare_two_final_csvs.py
+++ validation/comparison_of_final_files/compare_two_final_csvs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5322ea9d3797b556805a06150c755f9acb04f8606726fb054b706d3fde679e2e
-size 10267
+oid sha256:95857149a334bbccb29d26eee051dac7ba5148b032ef1127ceb3561150968eb3
+size 10828
