370c0176f3f1e27cc50ce6fa948e3b44160e2d22
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-06-28 20:20:00 +0200

    NOTICKET: Exclude source id

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95d2cce3084ac9e11a97752178bfb6ef5819acafcf7a0269cc33d19ba47b711a
-size 12157
+oid sha256:0573afcd79e32d0f8a5c5347fbe7173fa332e1d4b292fbfcea099fad441a4bf1
+size 12178
