e2733549299fd917035ba4e7e47f356c1362113e
@gramlich1 <michael.gramlich@rtl-extern.de>
2023-06-29 10:27:17 +0200

    NOTICKET: Fix bug

--- validation/comparison_of_final_files/compare_two_final_csvs.py
+++ validation/comparison_of_final_files/compare_two_final_csvs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95857149a334bbccb29d26eee051dac7ba5148b032ef1127ceb3561150968eb3
-size 10828
+oid sha256:8b6ca40e692a1da3ae76163f2ef4cef7631f56c4cb0ad81526e93315848f5f7c
+size 10834
