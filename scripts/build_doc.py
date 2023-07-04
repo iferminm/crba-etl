d7d8decc86f6d88a71319dc0b0f3a26b997fcf26
MajorDaxx <37327717+MajorDaxx@users.noreply.github.com>
2023-07-04 09:02:03 +0200

    make buid_doc compatible with windows

--- scripts/build_doc.py
+++ scripts/build_doc.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04c6908982231d8522415613bb25fece58b43e7f1cde9e14640ee85fa2410e40
-size 6337
+oid sha256:41741ba5c7f5bda698a4e14ecc8ed2c6f52ed54eeeb049ae2ce068e45283bc30
+size 6376
