bf906b4d866eea557fad981812ed5eabe2d1d5ea
Israel Montilla <montilla.israel@bcg.com>
2025-07-18 14:51:58 +0400

    Lets selenium deal with the webdriver and adds valid options for the webdriver

--- etl/source_adapter/ilo.py
+++ etl/source_adapter/ilo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c696c4940141526f92b3c26a5fc276f26ca6eb3f063c9ba0d2c0dba5abd5b5e9
-size 4087
+oid sha256:5ec0bf33591c05b46ee27b7c05dd788aa7b853b8f279f0dde624fa99d4e00446
+size 4179
--- etl/source_adapter/nrgi.py
+++ etl/source_adapter/nrgi.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f9a79e6888f095df3a03af650ad1f577a8bfc0c40c043bdb378bb74860562b1
-size 4044
+oid sha256:c050c815fff4356f50aa731f1b3aaf84f9d5ea9b92bd79978485a95476423413
+size 4078
