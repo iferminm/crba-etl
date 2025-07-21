1adbbee4c794521cb1eb2755be138790259b502b
Israel Montilla <montilla.israel@bcg.com>
2025-07-21 09:58:28 +0400

    Makes relative import

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba68120a2ee0f94792ae8a3eacb03d5b7451dae079b58ddd091312d4580ab98
-size 13282
+oid sha256:0a3c82e403edaa51af1fd80d89d8c4e7588bcdbea8973cf017c961c2e51fae48
+size 13284
