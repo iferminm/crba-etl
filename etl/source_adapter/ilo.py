4cf6ae63ac370870ce036de045cf82bca438f740
Israel Montilla <montilla.israel@bcg.com>
2025-07-21 16:07:12 +0400

    Arranges imports

--- etl/source_adapter/ilo.py
+++ etl/source_adapter/ilo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6386057d45d0c9c644d12bc8fcdb40d20a4dabeff8a49d85089571afc9efb0e2
-size 3974
+oid sha256:0dbb12e3c0d93b76f80bb19874465cb6cbcec853ea98b723e9722d458f9e72ea
+size 3975
