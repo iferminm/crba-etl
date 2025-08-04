0f3cb11262a705f0a9a55013aacb6a132dffb272
Israel Montilla <montilla.israel@bcg.com>
2025-08-04 12:42:30 +0400

    Fixes sources X-162 and X-163

--- etl/methology/column_mapping.py
+++ etl/methology/column_mapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da53f09d65a0592fdc6a3a2e7b4fc2ea00f1db350ac64a9f74df747ed8887184
-size 13251
+oid sha256:f0ce13993b92eef023ecdd72dc44e895eec4da55d1801df5ed7cbf8c2fb72fe3
+size 13376
