7c740ac8c9f210f6c923420ba13f6bdb35881aab
Israel Montilla <montilla.israel@bcg.com>
2025-08-04 13:38:00 +0400

    Fixes X-171 and X-209

--- etl/methology/column_mapping.py
+++ etl/methology/column_mapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0ce13993b92eef023ecdd72dc44e895eec4da55d1801df5ed7cbf8c2fb72fe3
-size 13376
+oid sha256:36345f7c8667cd36333157a9a6ca76d665cdb9539ecfdd5a091843b23eafabb8
+size 13462
