516baf894a693f3c303324f244c95976b30e418d
El Harzli Ouns <elharzli.ouns@bcg.com>
2025-09-09 11:53:58 +0200

    inverted scores

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:866c97bd35e0b5432a9c7e5576759bc21009d54f7d930a3ec25ea50a47677f9e
-size 13033
+oid sha256:5e844916428cd5a48b14bffa13e9a0a898bb4dd2fe518ab8f5ac06a1e3e27541
+size 13419
