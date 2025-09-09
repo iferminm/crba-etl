9b56c524f6bfd52d2e8091bdcdfb26e3816e0780
El Harzli Ouns <elharzli.ouns@bcg.com>
2025-09-09 12:25:48 +0200

    fix

--- etl/run.py
+++ etl/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e844916428cd5a48b14bffa13e9a0a898bb4dd2fe518ab8f5ac06a1e3e27541
-size 13419
+oid sha256:f1c159425fa64050f741bc7f12cf69a6a6c2fe78180e3d0df290e87a0c2ed8c7
+size 13335
